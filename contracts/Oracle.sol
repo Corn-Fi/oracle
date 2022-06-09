db874f0594be3c32eab9f8e0ac927e96776fee74
Korey Corkscrew <77598913+korey-corkscrew@users.noreply.github.com>
2022-06-09 01:49:53 -0500

    fixed decimals

--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -14,26 +14,29 @@ contract Oracle {
     uint8 public constant decimals = 6;
 
     function getRateUSD(IERC20 token) public view returns (uint256) {
-        uint256 rateUSDC = oracle.getRate(token, USDC, true);
         ( , int256 answer, , , ) = usdcOracle.latestRoundData();
+        if(token == USDC) {
+            return uint256(answer) / 1e2;
+        }
+        uint256 rateUSDC = oracle.getRate(token, USDC, true);
+        uint256 denominator = 10 ** (18 - IERC20Metadata(address(token)).decimals());
+        rateUSDC /= denominator;
         return (rateUSDC * 1e8) / uint256(answer);
     }
 
     function getLpRateUSD(IUniswapV2Pair lpToken) public view returns (uint256) {
-        ( , int256 answer, , , ) = usdcOracle.latestRoundData();
-
         IERC20Metadata token0 = IERC20Metadata(lpToken.token0());
         IERC20Metadata token1 = IERC20Metadata(lpToken.token1());
         (uint112 reserve0, uint112 reserve1, ) = lpToken.getReserves();
 
-        uint256 rate0USDC = (oracle.getRate(token0, USDC, true) * 1e8) / uint256(answer);
-        uint256 rate1USDC = (oracle.getRate(token1, USDC, true) * 1e8) / uint256(answer);
+        uint256 rate0USD = getRateUSD(token0);
+        uint256 rate1USD = getRateUSD(token1);
 
         uint256 denominator0 = 10 ** token0.decimals();
         uint256 denominator1 = 10 ** token1.decimals();
 
-        uint256 token0TotalUSD = (rate0USDC * reserve0) / denominator0;
-        uint256 token1TotalUSD = (rate1USDC * reserve1) / denominator1;
+        uint256 token0TotalUSD = (rate0USD * reserve0) / denominator0;
+        uint256 token1TotalUSD = (rate1USD * reserve1) / denominator1;
 
         uint256 totalUSD = token0TotalUSD + token1TotalUSD;
 
